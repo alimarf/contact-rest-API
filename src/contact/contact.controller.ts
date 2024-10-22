c63b939903410a34cd59bbc9a6f275d89ad78d08
alimarf <alimarief97@gmail.com>
2024-10-22 23:03:34 +0700

    feat: create & get contact API done

---
+++ src/contact/contact.controller.ts
@@ -0,0 +1,43 @@
+import { WebResponse } from './../model/web.model';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { ContactService } from './contact.service';
+import { Auth } from '../common/auth.decorator';
+import { ContactResponse, CreateContactRequest } from '../model/contact.model';
+import { User } from '@prisma/client';
+
+@Controller('/api/contacts')
+export class ContactController {
+  constructor(private contactService: ContactService) {}
+
+  @Post()
+  @HttpCode(200)
+  async create(
+    @Auth() user: User,
+    @Body() request: CreateContactRequest,
+  ): Promise<WebResponse<ContactResponse>> {
+    const result = await this.contactService.create(user, request);
+    return {
+      data: result,
+    };
+  }
+
+  @Get('/:contactId')
+  @HttpCode(200)
+  async get(
+    @Auth() user: User,
+    @Param('contactId', ParseIntPipe) contactId: number,
+  ): Promise<WebResponse<ContactResponse>> {
+    const result = await this.contactService.get(user, contactId);
+    return {
+      data: result,
+    };
+  }
+}
--- test/test.service.ts
+++ test/test.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../src/common/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { Contact } from '@prisma/client';
 
 @Injectable()
 export class TestService {
@@ -8,6 +9,14 @@ export class TestService {
 
   async deleteUser() {
     await this.prismaService.user.deleteMany({
+      where: {
+        username: 'test',
+      },
+    });
+  }
+
+  async deleteContact() {
+    await this.prismaService.contact.deleteMany({
       where: {
         username: 'test',
       },
@@ -29,6 +38,26 @@ export class TestService {
         name: 'test',
         password: await bcrypt.hash('test', 10),
         token: 'test',
+      },
+    });
+  }
+
+  async getContact(): Promise<Contact> {
+    return await this.prismaService.contact.findFirst({
+      where: {
+        username: 'test',
+      },
+    });
+  }
+
+  async createContact() {
+    await this.prismaService.contact.create({
+      data: {
+        first_name: 'test',
+        last_name: 'test',
+        email: 'test@example.com',
+        phone: '9999',
+        username: 'test',
       },
     });
   }
